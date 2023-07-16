e12a0ae5253ca08762dae599a4b507199e3b3d53
MesfinGithinji <meshgithinji@gmail.com>
2023-07-17 02:27:12 +0300

    Dictionary attributes updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,6 +58,45 @@ class FileStorage:
 
         return class_dict
 
+    def attributes(self):
+        """Returns a dictionary of attributes and their types based in class_name"""
+
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
+
     def reload(self):
         """
         Deserializes the JSON file and updates the objects dictionary.
